f9dd6e7f34daad5d856ef40dd2c4bdb82a72555a
Amin Ghasvari <amin@spcuinc.com>
2021-09-03 18:36:11 +0430

    Add writing in a file functionality for the logger

--- log/log.go
+++ log/log.go
@@ -1,7 +1,9 @@
 package spcuLog
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
+	. "os"
 )
 
 // Time format for our log package
@@ -11,6 +13,8 @@ const iso8601DateFormat = "2006-01-02T15:04:05"
 // It specifies the details about a new SpcuLogger
 type SpcuLoggerConfig struct {
 	IsProductionMode bool `default:"true"`
+	IsReportCaller   bool `default:"false"`
+	OutputPath       *string
 }
 
 // SpcuLogger
@@ -22,21 +26,29 @@ type SpcuLogger struct {
 
 // NewLogger
 // It is to create new SpcuLogger by a SpcuLoggerConfig.
-func NewLogger(config SpcuLoggerConfig) *SpcuLogger {
+func NewLogger(config SpcuLoggerConfig) (*SpcuLogger, error) {
 	// Create new logrus logger
-	logrusLogger := logrus.New()
-	logrusLogger.SetLevel(logrus.InfoLevel)
-	logrusLogger.SetFormatter(&logrus.TextFormatter{
+	ll := logrus.New()
+	ll.SetLevel(logrus.InfoLevel)
+	ll.SetFormatter(&logrus.TextFormatter{
 		ForceColors:     true,
 		TimestampFormat: iso8601DateFormat,
 	})
 	// Adds file and line number
-	logrusLogger.SetReportCaller(true)
+	ll.SetReportCaller(config.IsReportCaller)
+	// Check if it is going to be saved on file
+	if config.OutputPath != nil {
+		f, err := OpenFile(*config.OutputPath, O_RDWR|O_CREATE|O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("Could Not Open Log File : " + err.Error())
+		}
+		ll.SetOutput(f)
+	}
 
-	logrusLogger.Infof("Log package has been provided for:\n%+v", config)
+	ll.Infof("A SPCU logger has been provided: %+v\n", config)
 
 	return &SpcuLogger{
-		logrusLogger,
+		ll,
 		config,
-	}
+	}, nil
 }
--- log/log_test.go
+++ log/log_test.go
@@ -3,7 +3,28 @@ package spcuLog
 import "testing"
 
 func TestSpcuLogger(t *testing.T) {
-	logger := NewLogger(SpcuLoggerConfig{})
+	logger, err := NewLogger(SpcuLoggerConfig{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	logger.Traceln("This is a trace log.")
+	logger.Println("This is a print mode.")
+	logger.Infoln("This is an information log.")
+	logger.Warningln("This is a warning log.")
+	logger.Warnln("This is a warn log.")
+	logger.Debugln("This is a debug log.")
+	logger.Errorln("This is an error log.")
+}
+
+func TestSpcuLoggerWithFileOutput(t *testing.T) {
+	logPath := "temp.log"
+	logger, err := NewLogger(SpcuLoggerConfig{
+		IsReportCaller: true,
+		OutputPath:     &logPath,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 	logger.Traceln("This is a trace log.")
 	logger.Println("This is a print mode.")
 	logger.Infoln("This is an information log.")
