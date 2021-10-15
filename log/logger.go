74c8da7c41d4e591f2e4382c6e8829d5da494c43
Amin Ghasvari <amin@spcuinc.com>
2021-10-15 23:13:04 +0330

    Add File Threshold Hook

--- log/logger.go
+++ log/logger.go
@@ -3,6 +3,7 @@ package spcuLog
 import (
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"math"
 	. "os"
 )
 
@@ -14,7 +15,34 @@ const iso8601DateFormat = "2006-01-02T15:04:05"
 type SpcuLoggerConfig struct {
 	IsProductionMode bool `default:"true"`
 	IsReportCaller   bool `default:"false"`
-	OutputPath       *string
+	OutputFileConfig *OutputFileConfig
+}
+
+// OutputFileConfig
+// To specify more details about saving logs in files
+type OutputFileConfig struct {
+	Path string
+	Name string
+	// Default is storing logs in 100KB files
+	MaxSizeInBytes int64
+	// Default is storing logs in 10 files
+	MaxNumberOfFiles int `default:"10"`
+}
+
+func (ofc OutputFileConfig) FullPath(fileIndex int) string {
+	if ofc.Name == "" {
+		ofc.Name = "log_file"
+	}
+
+	// Calculate number of digits
+	var numberOfDigits int
+	if ofc.MaxNumberOfFiles == 1 {
+		numberOfDigits = 1
+	} else {
+		numberOfDigits = int(math.Log10(float64(ofc.MaxNumberOfFiles)-1) + 1)
+	}
+
+	return fmt.Sprintf("%s/%s-%0*d.log", ofc.Path, ofc.Name, numberOfDigits, fileIndex)
 }
 
 // SpcuLogger
@@ -37,18 +65,17 @@ func NewLogger(config SpcuLoggerConfig) (*SpcuLogger, error) {
 	// Adds file and line number
 	ll.SetReportCaller(config.IsReportCaller)
 	// Check if it is going to be saved on file
-	if config.OutputPath != nil {
-		f, err := OpenFile(*config.OutputPath, O_RDWR|O_CREATE|O_APPEND, 0666)
+	if config.OutputFileConfig != nil {
+		f, err := OpenFile((*config.OutputFileConfig).FullPath(0), O_RDWR|O_CREATE|O_APPEND, 0666)
 		if err != nil {
 			fmt.Println("Could Not Open Log File : " + err.Error())
 		}
 		ll.SetOutput(f)
+
+		// For checking storage limitation
+		ll.AddHook(NewFileThresholdHook(*config.OutputFileConfig))
 	}
-
 	ll.Infof("A SPCU logger has been provided: %+v\n", config)
-
-	// For now
-
 
 	return &SpcuLogger{
 		ll,
--- log/logger_test.go
+++ log/logger_test.go
@@ -8,7 +8,6 @@ func TestSpcuLogger(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	logger.Traceln("This is a trace log.")
-	logger.Println("This is a print mode.")
 	logger.Infoln("This is an information log.")
 	logger.Warningln("This is a warning log.")
 	logger.Warnln("This is a warn log.")
@@ -17,16 +16,17 @@ func TestSpcuLogger(t *testing.T) {
 }
 
 func TestSpcuLoggerWithFileOutput(t *testing.T) {
-	logPath := "temp.log"
 	logger, err := NewLogger(SpcuLoggerConfig{
 		IsReportCaller: true,
-		OutputPath:     &logPath,
+		OutputFileConfig: &OutputFileConfig{
+			Path:             ".",
+			MaxNumberOfFiles: 10,
+		},
 	})
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	logger.Traceln("This is a trace log.")
-	logger.Println("This is a print mode.")
 	logger.Infoln("This is an information log.")
 	logger.Warningln("This is a warning log.")
 	logger.Warnln("This is a warn log.")
@@ -34,6 +34,23 @@ func TestSpcuLoggerWithFileOutput(t *testing.T) {
 	logger.Errorln("This is an error log.")
 }
 
-func TestSpcuLoggerFileThreshold(t *testing.T)  {
+func TestSpcuLoggerFileThreshold(t *testing.T) {
+	logger, err := NewLogger(SpcuLoggerConfig{
+		IsReportCaller: true,
+		OutputFileConfig: &OutputFileConfig{
+			Path:             ".",
+			Name:             "counter",
+			MaxNumberOfFiles: 20,
+			MaxSizeInBytes:   256000,
+		},
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 
+	for i := 0; i < 50000; i++ {
+		logger.Infoln(i, "This is an information log.")
+		logger.Warnln(i, "This is a warn log.")
+		logger.Errorln(i, "This is an error log.")
+	}
 }
